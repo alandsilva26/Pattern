bf678ba7f76efdca3615b9cdf7a1d935326cd98c
DeathBringer26 <43207460+DeathBringer26@users.noreply.github.com>
2019-02-19 12:14:03 +0530

    created new program

---
+++ 05.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,j,k=1,n,c;
+	//prompt user to enter a number and read input
+	printf("Enter a number:");
+	scanf("%d",&n);
+	//creates copy
+	c=n;
+	//first loop iterates through the rows 
+	for(i=1;i<=n;i++)
+	{
+		//second loop iterates through each coloum in each row
+		for(j=1;j<=n;j++)
+		{
+			//the first number is printed by value of c i.e. the last number
+			printf("%d\t",c);
+			//incrementing k
+			c--;
+		}
+		printf("\n");\
+		//reset the value of c
+		c=n;
+	}
+	getch();
+	return 0;
+}
