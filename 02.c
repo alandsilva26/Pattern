7e6f5d4c98828bd638726bad6f21b24961260b5a
DeathBringer26 <43207460+DeathBringer26@users.noreply.github.com>
2019-02-20 19:01:04 +0530

    changed n to rows

--- 02.c
+++ 02.c
@@ -7,13 +7,13 @@
 #include<conio.h>
 int main()
 {
-	int i,j,k=1,n=5;
+	int i,j,k=1,rows;
 	//same as 01 but the values printed in each row is equal to #of row 
 	//eg. 1 has 1
 	//eg. 2 has 1 2
 	printf("Enter a number:");
-	scanf("%d",&n);
-	for(i=1;i<=n;i++)
+	scanf("%d",&rows);
+	for(i=1;i<=rows;i++)
 	{
 		for(j=1;j<=i;j++)
 		{
