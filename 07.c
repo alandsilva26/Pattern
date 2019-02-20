9bf982b7a028e8371b3cd26d53044162bd382a39
DeathBringer26 <43207460+DeathBringer26@users.noreply.github.com>
2019-02-20 19:39:58 +0530

    added code

--- 07.c
+++ 07.c
@@ -8,7 +8,23 @@
 #include<conio.h>
 int main()
 {
-	int 
+	int i,j,k=1,rows;
+	printf("Enter number of rows:");
+	scanf("%d",&rows);
+	for(i=1;i<=rows;i++)
+	{
+		for(j=1;j<rows-i;j++)
+		{
+			printf(" \t");
+		}
+		for(j=1;j<=(2*i-1);j++)
+		{
+			prinf("k\t");
+			k++
+		}
+		k=1;
+	 	printf("\n");
+	}
 	getch();
 	return 0;
 }
