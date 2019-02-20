02413bc495636d8e96b39f5e95fab89d24804c1d
DeathBringer26 <43207460+DeathBringer26@users.noreply.github.com>
2019-02-20 18:56:24 +0530

    updated

--- 06.c
+++ 06.c
@@ -15,26 +15,16 @@ int main()
 	//One to print numbers
 
 
-	/*
-	This for loop has a special conditions since the number of rows printed is not equal to the number entered by the user
-	*/
-    for(i=1;i<(2*n-1)/2;i++)
-	{  
-		//prints spaces
-		for(j=1;j<n-1-i;j++)
+	for()
+	{
+		for()
 		{
-			printf(" \t");
+
 		}
-		//here the count of numbers printed is odd
-		//hence we use the condition for odd numbers
-		for(j=1;j<=(2*i-1);j++)
+		for()
 		{
-			printf("%d\t",k);
-			k++;
+			
 		}
-		//reset value of k
-		k=1;
-		printf("\n");
 	}
 	getch();
 	return 0;
