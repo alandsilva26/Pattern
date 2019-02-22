1a5f8c71dc3dc27a1da9c540c8f0c3d019fe9166
DeathBringer26 <43207460+DeathBringer26@users.noreply.github.com>
2019-02-22 20:48:39 +0530

    added coments

--- 07.c
+++ 07.c
@@ -13,15 +13,30 @@ int main()
 	scanf("%d",&rows);
 	for(i=1;i<=rows;i++)
 	{
+		//prints spaces
 		for(j=1;j<=rows-i;j++)
 		{
 			printf(" \t");
 		}
-
+		//prints the first half of numbers
+		/*
+				1
+			  1 2 
+            1 2 3
+          1 2 3 4 
+		*/
 		for(j=1;j<=i;j++)
 		{
 			printf("%d\t",j);
 		}
+		//prints the second half
+		/*
+				
+				 1
+				 2 1 
+                 3 2 1
+			     4 3 2 1
+		*/
 		for(j=i-1;j>0;j--)
 		{
 			printf("%d\t",j);
