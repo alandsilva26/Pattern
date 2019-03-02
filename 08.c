c5bb185a8866d3b3e71ffd0b3f22d790c6696275
DeathBringer26 <43207460+DeathBringer26@users.noreply.github.com>
2019-03-02 13:22:36 +0530

    updated logic

--- 08.c
+++ 08.c
@@ -4,7 +4,23 @@ int main()
 {
 	int i,j,rows;
 	printf("Enter Number of rows:");
+	scanf("%d",&rows);
 
-	getch()
+	for(i=1;i<=rows;i++)
+	{
+		for(j=1;j<=rows-i;j++)
+		{
+			printf("\t");
+		}
+		//here j is already at the number that we need to print
+		//thus we just need to set the condition 
+		for(j;j<=rows;j++)
+		{
+			printf("%d\t",j);
+		}
+		printf("\n");
+	}
+
+	getch();
 	return 0;
 }
