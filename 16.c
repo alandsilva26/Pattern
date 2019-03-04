4280f387f1fb7c6c4e2911a7edacda70973ee0b1
DeathBringer26 <43207460+DeathBringer26@users.noreply.github.com>
2019-03-04 09:42:26 +0530

    Update 16.c

--- 16.c
+++ 16.c
@@ -9,7 +9,23 @@
 #include<conio.h>
 int man()
 {
-	int 
+	int i,j,k,rows;
+	printf("Enter number of rows:");
+	scanf("%d",&rows);
+	for(i=0;i<=rows;i++)
+	{
+		for(j=1;j<=rows-i;j++)
+		{
+			printf("\t'");
+		}
+		k=rows;
+		for(j=1;j<=i;j++)
+		{
+			printf("%d\t",k);
+			k--;
+		}
+		printf("\n");
+	}
 	getch();
 	return 0;
 }
