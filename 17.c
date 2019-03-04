ae95794fc9a6efcae56dfb4ad6d7fc44b04c6d56
DeathBringer26 <43207460+DeathBringer26@users.noreply.github.com>
2019-03-04 09:38:59 +0530

    added basic template

---
+++ 17.c
@@ -0,0 +1,15 @@
+/* 
+* * * * * * * * *   
+* * * *   * * * *  
+* * *       * * *  
+* *           * *  
+*               *  
+*/
+#include<stdio.h>
+#include<conio.h>
+int man()
+{
+	int 
+	getch();
+	return 0;
+}
