31c68f4036b8795f61fb0ca9c9ae5859fea312d4
DeathBringer26 <43207460+DeathBringer26@users.noreply.github.com>
2019-03-05 20:02:28 +0530

    Update 17.c

--- Programs/17.c
+++ Programs/17.c
@@ -9,4 +9,37 @@
 #include<conio.h>
 int main()
 {
+	int i,j,rows;
+	printf("Enter number of rows:");
+	scanf("%d",&rows);
+
+	for(i=1;i<=rows;i++)
+	{
+		for(j=1;j<=rows+1-i;j++)
+		{
+			printf("*\t");
+		}
+		for(j=1;j<=2*(i-1)-1;j++)
+		{
+			printf("\t");
+		}
+		for(j=1;j<=rows+1-i;j++)
+		{
+			/*
+			here the first and second row of the second part have the same number of rows
+			thus when we print the first row the number of stars printed are one less
+			*/
+			if(i==1 && j==1)
+			{
+				j++;
+			}
+			else
+			{
+				printf("*\t");
+			}
+		}
+		printf("\n");
+	}
+	getch();
+	return 0;
 }
