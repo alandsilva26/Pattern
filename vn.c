4807d5d5d9c9be710a21db7293bc3198fba4d6a0
Alan Dsilva <deathbringer2604@gmail.com>
2019-03-08 12:04:48 +0530

    Create vn.c
    change name and add code

---
+++ vn.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main()
+{
+	int i,j,rows,k;
+	printf("Enter number of rows:");
+	scanf("%d",&rows);
+	
+	for(i=1;i<=rows;i++)
+	{
+		k=i;
+		for(j=1;j<=rows;j++)
+		{
+			printf("%d\t",k);
+			k=k+rows;
+		}
+		printf("\n");
+	}
+	getch();
+    return 0;
+}
