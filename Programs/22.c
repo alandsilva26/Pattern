f39e8295695a0dfa3c3534a66388e4604f14b3aa
Alan <alandsilva2001@gmail.com>
2019-03-17 13:43:59 +0530

    Update 22.c

--- Programs/22.c
+++ Programs/22.c
@@ -5,31 +5,8 @@ int main()
 	int n,i,j,k,p;
 	int c;
 	k=0;
-	clrscr();
 	printf("Enter a number:");
 	scanf("%d",&n);
-	/*
-	for(i=n;i>=0;i--)
-		{
-			c=1;
-			for(j=i;j>0;j--)
-				{
-					printf("%d\t",c);
-					c++;
-				}
-			printf("\n");
-		}
-	for(i=1;i<=n;i++)
-		{
-			c=n;
-			for(j=1;j<=n;j++)
-				{
-					printf("%d\t",c);
-					c--;
-				}
-			printf("\n");
-		}              */
-		printf("\n\n");
 	for(i=1;i<=n;i++)
 		{
 			k=k+i;
